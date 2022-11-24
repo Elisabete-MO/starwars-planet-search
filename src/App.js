35aa64506a72b750e460d894dd3d6021a5a07221
Bete.mrt <bete.mrt@gmail.com>
2022-11-24 20:39:21 -0300

    estrutura básica: css, context, fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Loading from './pages/Loading';
+import Header from './components/Header';
+import PlanetsForm from './components/PlanetsForm';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>StarWars</span>
+    <>
+      <Switch>
+        <Route path="/loading" component={ Loading } />
+        {/* {<Route exact path="/" component={ App } /> */}
+      </Switch>
+      <div>
+        <Header />
+        <PlanetsForm />
+        <Table />
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import StarWarsProvider from './context/StarWarsProvider';
 import './index.css';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />);
+  .render(
+    <BrowserRouter>
+      <StarWarsProvider>
+        <App />
+      </StarWarsProvider>
+    </BrowserRouter>,
+  );
